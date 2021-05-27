94128524c19e01a4b27d5ffdd981d05417f8334b
guidezena <guilhermepereiradezena@gmail.com>
2021-05-27 15:49:57 -0300

    aula 14-05

--- routes/web.php
+++ routes/web.php
@@ -27,7 +27,7 @@ Route::get('/avisos', function () {
                 ['id'=> 2, 'texto'=>'Feriado semana que vem']]]);
 });
 
-Auth::routes();
+
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index']);
