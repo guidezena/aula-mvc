de3c1d4ed608290a7e39ee040325a575092117c4
guidezena <guilhermepereiradezena@gmail.com>
2021-06-04 13:34:30 -0300

    commit aula-28-05

--- app/Http/Controllers/ClientesController.php
+++ app/Http/Controllers/ClientesController.php
@@ -4,78 +4,145 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Clientes;
-use App\Models\Funcionario;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Spatie\Permission\Models\Role;
-use Spatie\Permission\Models\HasRoles;
+use Spatie\Permission\Traits\HasRoles;
 
 class ClientesController extends Controller
 {
     use HasFactory;
-    
+    use HasRoles;
 
-    public function __construct()
+	
+	//Essa é uma forma de controlar o acesso
+	public function __construct()
+	{
+		$this->middleware('permission:cliente-list',['only' => ['index','show']]);
+		$this->middleware('permission:cliente-create',['only' => ['create','store']]);
+		$this->middleware('permission:cliente-edit',['only' => ['edit','update']]);
+		$this->middleware('permission:cliente-delete',['only' => ['destroy']]);
+	}
+
+    public function getCliente($id)
     {
-        $this->middleware('permission:cliente-list',['only' => ['index','show']]);
-        $this->middleware('permission:cliente-create',['only' => ['create','store']]);
-        $this->middleware('permission:cliente-edit',['only' => ['edit','update']]);
-        $this->middleware('permission:cliente-delete',['only' => ['destroy']]);
-        
-        //$this->middleware('auth');
+        $cliente = Clientes::find($id);
+
+        return $cliente;
     }
-    public function listar(){
-        $clientes = Clientes::all();
-        return view('clientes.listar', ['clientes' => $clientes]);
+
+    public function checkCliente(int $id)
+    {
+        $cliente = Clientes::find($id);
+
+        return $cliente ?? false;
     }
-    public function index( Request $request){
+
+    public function listar()
+    {
+    	$clientes = Clientes::all();
+
+    	return view('clientes.listar', ['clientes' => $clientes]);
+    }
+
+    public function index( Request $request)
+    {
         $qtd_por_pagina = 5;
-        $data = Clientes::orderBy('id','DESC')->paginate($qtd_por_pagina);
-        return view('clientes.index',compact('data'))->with('i',($request->input('page', 1) - 1) * $qtd_por_pagina);
+
+        $data = Clientes::orderBy('id', 'DESC')->paginate($qtd_por_pagina);
+
+        return view('clientes.index', 
+                compact('data'))->
+                    with('i', ($request->input('page', 1) - 1) * $qtd_por_pagina);
     }
 
-    public function create(){
-        $roles = Role::pluck('name','name')->all();
-        return view('users.create', compact($roles));
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('clientes.create');
     }
 
-    public function store(Request $request){
-        $this->validate($request,['nome'=>'required', 'email' => 'required|email|unique:users,email']);
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request,
+                        ['nome' => 'required',
+                        'email' => 'required|email|unique:users,email']);
 
         $input = $request->all();
 
-        $user = Clientes::create($input);
+        Clientes::create($input);
 
-        //$user->assignRole($request->input('roles'));
-
-        return redirect()->route('clientes.index')->width('success','Cliente criado com sucesso');
+        return redirect()->route('clientes.index')->with('success','Cliente criado com sucesso');
     }
 
-    public function show($id){
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
         $cliente = Clientes::find($id);
+
         return view('clientes.show', compact('cliente'));
     }
 
-    public function edit($id){
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
         $cliente = Clientes::find($id);
-        //$roles = Role::pluck('name','name')->all();
-        //$clienteRole = $cliente->roles->pluck('name','name')->all();
-        
-    return view('clientes.edit', compact('cliente'/*,'role','clienteRole'*/));
+
+        return view('clientes.edit', compact('cliente'));
     }
 
-    public function update(Request $request, $id){
-        $this->validate($request,['nome'=>'required',
-                                  'email'=>'required|email|unique:users,email'
-                                    ]);
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $this->validate($request,
+                        ['nome' => 'required',
+                        'email' => 'required|email|unique:users,email']);
 
         $input = $request->all();
+
         $cliente = Clientes::find($id);
+
         $cliente->update($input);
+
+        return redirect()->route('clientes.index')->with('success', 'Cliente atualizado com sucesso');
     }
 
-    public function destroy($id){
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
         Clientes::find($id)->delete();
 
         return redirect()->route('clientes.index')->with('success','Cliente removido com sucesso');
-    }
+    }    
+
 }
